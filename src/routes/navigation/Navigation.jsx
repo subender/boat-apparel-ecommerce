f30b4a16ac395135343d36d2f8d7be375d4aa982
subender <subender.io@outlook.com>
2022-05-09 01:57:43 +0530

    added navbar

---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import logo from '../../assets/logo.svg'
+import './Navigation.styles.scss'
+
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+ 
+        <Link className="logo-container" to="/">
+        <img src={logo} alt="logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
