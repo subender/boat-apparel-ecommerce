b8f8fd2e4ceb1cd0c5cfa64667438a4f0af44d6e
subender <subender.io@outlook.com>
2022-05-19 04:17:15 +0530

    added navigation of directory

--- src/components/category-items/CategoryItem.jsx
+++ src/components/category-items/CategoryItem.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import './CategoryItem.styles.scss'
 const CategoryItem = ({category}) => {
 
-  const {imageUrl, title} = category;
+  const {imageUrl, title, route} = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = ()=>{
+    navigate(route);
+    // console.log(`shop/${title}`);
+  }
 
   return (
    
-    <div className="category__container">
+    <div className="category__container" onClick={onNavigateHandler}>
       <div className="category__bg--img" style={{backgroundImage: `url(${imageUrl})`}} />
       <div className="category__body__container">
-        <h2>Hats</h2>
+        <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
       
